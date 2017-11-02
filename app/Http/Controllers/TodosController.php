e77ce9c7f696af6c9a4b89369315f43d09d3128d
Valentin Brosseau <c4software@gmail.com>
2017-11-02 22:29:50 +0100

    Add Empty controller

---
+++ app/Http/Controllers/TodosController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Todos;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+
+class TodoController extends Controller{
+  // Method here
+}

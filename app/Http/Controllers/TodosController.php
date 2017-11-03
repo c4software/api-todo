0a4fe12a091f34db23a9fe18897dc3b09c8ae7b2
Valentin Brosseau <c4software@gmail.com>
2017-11-03 22:59:42 +0100

    Add method to the controller

--- app/Http/Controllers/TodosController.php
+++ app/Http/Controllers/TodosController.php
@@ -8,5 +8,34 @@ use Illuminate\Http\Request;
 
 
 class TodoController extends Controller{
+  public function liste(){
+    $todos  = Todos::all();
+    return response()->json($todos);
+  }
 
+  public function saveTodo(Request $request){
+    $todo = Todos::create($request->all());
+    return response()->json($todo);
+  }
+
+  public function markAsDone($id){
+    $todo  = Todos::find($id);
+    if($todo){
+      $todo->termine = 1;
+      $todo->save();
+      return response()->json("success");
+    }else{
+      return response()->json("error");
+    }
+  }
+
+  public function deleteTodo($id){
+    $todo  = Todos::find($id);
+    if($todo && $todo->termine){
+      $todo->delete();
+      return response()->json('success');
+    }else{
+      return response()->json('error');
+    }
+  }
 }
